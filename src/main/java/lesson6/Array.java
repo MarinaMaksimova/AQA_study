50ef4ee425596d40d4ce79a5a09e4aab61efa6b7
Marina <condra_77@mail.ru>
2024-11-13 21:26:39 +0300

    Решение задания lesson6

---
+++ src/main/java/lesson6/Array.java
@@ -0,0 +1,81 @@
+package lesson6;
+
+import java.util.Random;
+
+/*1. Напишите метод, на вход которого подается двумерный строковый массив размером 4х4. При подаче массива другого размера
+необходимо бросить исключение MyArray SizeException.
+2. Далее метод должен пройтись по всем элементам массива, преобразовать в int и просуммировать. Если в каком-то элементе
+массива преобразование не удалось (например, в ячейке лежит символ или текст вместо числа), должно быть брошено исключение
+MyArrayDataException с детализацией, в какой именно ячейке лежат неверные данные.
+3. В методе main() вызвать полученный метод, обработать возможные исключения MyArray SizeException и MyArrayDataException и
+вывести результат расчета.*/
+class MyArraySizeException extends Exception {
+    public MyArraySizeException(String message) {
+        super(message);
+    }
+}
+
+class MyArrayDataException extends Exception {
+    public MyArrayDataException(String message) {
+        super(message);
+    }
+}
+
+public class Array {
+    public String[][] array;
+    private int[][] arrayInt;
+    private int sumOfArray;
+
+    public Array(int rows, int columns) {           // конструктор класса
+        array = new String[rows][columns];
+        arrayInt = new int[rows][columns];
+    }
+
+    public void fillArrayWithNumbers() {           // метод случайного заполнения массива, где строки станут впоследствии цифрами
+        Random random = new Random();
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                array[i][j] = Integer.toString((int) (Math.random() * 9));
+            }
+        }
+    }
+
+    public void fillArrayWithSymbols() {           // метод случайного заполнения массива, где строки станут впоследствии буквами
+        Random random = new Random();
+        String symbols = "абвгдеёжзи0123456789";
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                int randomSymbol = random.nextInt(symbols.length());
+                array[i][j] = Character.toString(symbols.charAt(randomSymbol));
+            }
+        }
+    }
+
+    public void findSumOfArray(String[][] arr) throws MyArraySizeException, MyArrayDataException {         // тот самый нужный нам метод из задания
+        if (arr.length != 4 || arr[0].length != 4) {
+            throw new MyArraySizeException("Размер массива должен быть 4 на 4");
+        }
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                try {
+                    arrayInt[i][j] = Integer.parseInt(array[i][j]);
+                    sumOfArray += arrayInt[i][j];
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException("Невозможно преобразовать элемент в ячейке [" + i + "][" + j + "]: " + arr[i][j]);
+                }
+            }
+        }
+    }
+
+    public void showArray() {           // вывод массива в консоль
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("Сумма всех элементов в массиве: " + sumOfArray);
+    }
+
+
+}
--- src/main/java/lesson6/Main.java
+++ src/main/java/lesson6/Main.java
@@ -1,7 +1,24 @@
 package lesson6;
 
+/*1. Напишите метод, на вход которого подается двумерный строковый массив размером 4х4. При подаче массива другого размера
+необходимо бросить исключение MyArray SizeException.
+2. Далее метод должен пройтись по всем элементам массива, преобразовать в int и просуммировать. Если в каком-то элементе
+массива преобразование не удалось (например, в ячейке лежит символ или текст вместо числа), должно быть брошено исключение
+MyArrayDataException с детализацией, в какой именно ячейке лежат неверные данные.
+3. В методе main() вызвать полученный метод, обработать возможные исключения MyArray SizeException и MyArrayDataException и
+вывести результат расчета.*/
 public class Main {
     public static void main(String[] args) {
-
+        Array arr = new Array(4, 4);
+        arr.fillArrayWithSymbols();
+//        arr.fillArrayWithNumbers();
+        try {
+            arr.findSumOfArray(arr.array);
+            arr.showArray();
+        } catch (MyArraySizeException | MyArrayDataException e) {
+            //System.out.println(e.getMessage());
+        }
+//        arr.findSumOfArray(arr.array);
+//        arr.showArray();
     }
 }
